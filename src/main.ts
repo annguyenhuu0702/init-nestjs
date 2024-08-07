45d56d5af6a78692c857c711beb5e096de2ba282
nguyenhuuan2727 <nguyenhuuan2727@gmail.com>
2024-08-07 16:53:46 +0700

    init ci cd

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from '@commonfilter/global-exception.filter';
-import { LoggerService } from '@loggerlogger.service';
 import { setupSwagger } from '@config/swagger.config';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { LoggerService } from '@logger/logger.service';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
